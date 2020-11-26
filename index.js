c49e6103e6364c3cea274de996e6d7f7eca8be30
gbuisson <guillaume.buisson.simplon@gmail.com>
2020-11-26 21:07:13 +0100

    tolowercase

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 const prefix = "among-";
 const commands = {
     "tag": "vous ajoute le role AmongUsPlayeur",
-    "map{1/2/3}": "ajoute une images de toutes les maps sur le channel",
+    "map{1/2/3}": "ajoute une images de la maps sur le channel",
 };
 const maps = ["map1","map2","map3"];
 
@@ -11,11 +11,12 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     var TailleMembres = client.users.cache.size;
     var TailleServeurs = client.guilds.cache.size;
-    client.user.setActivity("Connecté - " + TailleServeurs + " serveur(s) et " + TailleMembres + " membre(s)");
+    client.user.setActivity("Connecté sur le serveur");
 });
 
 client.on('message', msg => {
-    if (msg.content === `${prefix}tag`) {
+    let message = msg.content.toLowerCase();
+    if (message === `${prefix}tag`) {
         let role = msg.guild.roles.cache.get('766370186747576370');
 
         if(!msg.member.roles.cache.has('766370186747576370')) {
@@ -27,11 +28,11 @@ client.on('message', msg => {
             msg.reply(`Role ${role.name} Retiré`);
         }
     }
-    else if (msg.content === `${prefix}map1` || msg.content === `${prefix}map2` || msg.content === `${prefix}map3`) {
-        msg.reply("maps:", {files: [
+    else if (message === `${prefix}map1` || msg.content === `${prefix}map2` || msg.content === `${prefix}map3`) {
+        msg.reply(msg.content.slice(6), {files: [
                 `./maps_images/${msg.content.slice(6)}.jpg`,]})
     }
-    else if (msg.content === `${prefix}help`) {
+    else if (message === `${prefix}help`) {
         for(const command in commands) {
             msg.reply(command);
         }
